29f23e17b7a3f912f97d1f51e2cb1952c67e40c4
Rescor <rescorent@gmail.com>
2022-04-26 00:09:34 +0300

    Added background and explosion animation
    Now enemies are destroyed with an explosion animation.
    
    Added a space background.

--- game.js
+++ game.js
@@ -1,27 +1,38 @@
-let tank = document.getElementById("tank");
-let weapon = document.getElementById("weapon");
-let enemy = document.getElementById("enemy");
-let score = 0;
+const hpElem            = document.querySelector("#hp");
+const scoreElem         = document.querySelector("#score");
+const tank              = document.getElementById("tank");
+const weapon            = document.getElementById("weapon");
+let enemy               = document.getElementById("enemy");
 
-let enemies = {
+let positionVertical    = parseInt(getComputedStyle(tank).top);
+let positionHorizontal  = parseInt(getComputedStyle(tank).left);
+
+let score               = 0;
+let hp                  = 3;
+
+let allEnemies;
+let allLightSips;
+
+let enemies             = {
     lightShip: {
         className: "lightShip",
         health: 10,
         color: "green",
     },
+
     boss: {
         className: "boss",
         health: 50,
         color: "blue",
     }
 }
-let allEnemies;
-let allLightSips;
 
-let positionVertical = parseInt(getComputedStyle(tank).top);
-let positionHorizontal = parseInt(getComputedStyle(tank).left);
 
-let scoreElem =  document.querySelector("#score");
+function refreshGameStatus() {
+    hpElem.innerHTML    = hp;
+    scoreElem.innerHTML = score;
+}
+refreshGameStatus();
 
 document.onkeydown = function(e) {
     if (e.code === "ArrowUp") {
@@ -63,13 +74,13 @@ function bulletMove(bullet) {
     allEnemies = document.getElementsByClassName("enemy");
 
     let bulletMoveInterval = setInterval( () => {
-        console.log("interval runned")
         bullet.style.left = positionHorizontal + 90 + count + "px";
         count += 3;
         for (let i = 0; i < allEnemies.length; i++) {
             if (parseInt(bullet.style.left) + 20 >= parseInt(getComputedStyle(allEnemies[i]).left) && parseInt(bullet.style.left) - 70 <= parseInt(getComputedStyle(allEnemies[i]).left)) {
                 if (parseInt(bullet.style.top) >= parseInt(getComputedStyle(allEnemies[i]).top) && parseInt(bullet.style.top) <= parseInt(getComputedStyle(allEnemies[i]).top) + 40) {
                     bullet.remove();
+                    explosion(allEnemies[i]);
                     allEnemies[i].remove();
                     scoreCounter(50);
                     clearInterval(bulletMoveInterval);
@@ -83,6 +94,15 @@ function bulletMove(bullet) {
             bullet.remove()
         };
     }, 4);
+}
+
+function explosion(enemy) {
+    let explosion = document.createElement("div");
+    explosion.classList.add("explosion");
+    explosion.style.top = enemy.style.top;
+    explosion.style.left = enemy.style.left;
+    document.body.appendChild(explosion);
+    setTimeout(() => {document.body.removeChild(explosion)}, 850);
 }
 
 function enemySpawner(type) {
@@ -104,8 +124,17 @@ function enemySpawner(type) {
 function enemiesMove() {
     allLightSips = document.getElementsByClassName("lightShip");    
     for (let i = 0; i < allLightSips.length; i++) {
+        // remove enemies behind the screen and take damage
+        if (parseInt(allLightSips[i].style.left) <= -40) {
+            allLightSips[i].remove();
+            hp-=1;
+            refreshGameStatus();
+        }
+
         allLightSips[i].style.left = parseInt(allLightSips[i].style.left) - 20 + "px";
     }
+    console.log(document.getElementById("space").style.backgroundPosition);
+    //document.getElementById("space").style.backgroundPosition = parseInt(document.getElementById("space").style.backgroundPosition) + 10 + "%";
 }
 
 let enemySpawnInterval = setInterval(() => {
